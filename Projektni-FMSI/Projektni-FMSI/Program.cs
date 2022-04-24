a773fc5ab0d6330c0acebe7e86e4632081de0dba
Novica Tepic <acivon@outlook.com>
2022-04-24 16:15:47 +0200

    Test commit

---
+++ Projektni-FMSI/Projektni-FMSI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projektni_FMSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello two");
+        }
+    }
+}
